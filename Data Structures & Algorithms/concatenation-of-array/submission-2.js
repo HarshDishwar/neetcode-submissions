2825ccc38e2233775e99dd48d9f5f0313c15d272
HarshDishwar <29996209+HarshDishwar@users.noreply.github.com>
2026-06-04 02:18:18 +0530

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+        // let n = nums.length-1
+        // while(i<2n){
+        //     ans[i]=
+        // }
+        let ans = nums.concat(nums)
+        return ans
+        
+    }
+}
