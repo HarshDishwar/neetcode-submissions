2c292612cb3ce2f4c3091558b27cf5731e0a047d
HarshDishwar <29996209+HarshDishwar@users.noreply.github.com>
2026-06-07 01:55:15 +0530

    Add: squares-of-a-sorted-array - submission-1

---
+++ Data Structures & Algorithms/squares-of-a-sorted-array/submission-1.js
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    sortedSquares(nums) {
+        for(let i = 0 ;i<nums.length;i++){
+            nums[i] = nums[i]* nums[i]
+        }
+        return nums.sort((a,b)=>a-b)
+    }
+}
